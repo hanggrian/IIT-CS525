588a3c3cd1bed7325231e597faa8c057d51f4de3
Hendra Anggrian <hendraanggrian@gmail.com>
2023-06-29 17:16:06 -0500

    Apply Google style guide

--- assign1/storage_mgr.h
+++ assign1/storage_mgr.h
@@ -3,41 +3,40 @@
 
 #include "dberror.h"
 
-/************************************************************
- *                    handle data structures                *
- ************************************************************/
+//region Handle data structures.
 typedef struct SM_FileHandle {
-	char *fileName;
-	int totalNumPages;
-	int curPagePos;
-	void *mgmtInfo;
+  char *fileName;
+  int totalNumPages;
+  int curPagePos;
+  void *mgmtInfo;
 } SM_FileHandle;
 
-typedef char* SM_PageHandle;
+typedef char *SM_PageHandle;
+//endregion
 
-/************************************************************
- *                    interface                             *
- ************************************************************/
-/* manipulating page files */
-extern void initStorageManager (void);
-extern RC createPageFile (char *fileName);
-extern RC openPageFile (char *fileName, SM_FileHandle *fHandle);
-extern RC closePageFile (SM_FileHandle *fHandle);
-extern RC destroyPageFile (char *fileName);
+//region Interfaces.
+/* Manipulating page files. */
+extern void initStorageManager(void);
+extern RC createPageFile(char *fileName);
+extern RC openPageFile(char *fileName, SM_FileHandle *fHandle);
+extern RC closePageFile(SM_FileHandle *fHandle);
+extern RC destroyPageFile(char *fileName);
 
-/* reading blocks from disc */
-extern RC readBlock (int pageNum, SM_FileHandle *fHandle, SM_PageHandle memPage);
-extern int getBlockPos (SM_FileHandle *fHandle);
-extern RC readFirstBlock (SM_FileHandle *fHandle, SM_PageHandle memPage);
-extern RC readPreviousBlock (SM_FileHandle *fHandle, SM_PageHandle memPage);
-extern RC readCurrentBlock (SM_FileHandle *fHandle, SM_PageHandle memPage);
-extern RC readNextBlock (SM_FileHandle *fHandle, SM_PageHandle memPage);
-extern RC readLastBlock (SM_FileHandle *fHandle, SM_PageHandle memPage);
+/* Reading blocks from disc. */
+extern RC readBlock(int pageNum, SM_FileHandle *fHandle, SM_PageHandle memPage);
+extern int getBlockPos(SM_FileHandle *fHandle);
+extern RC readFirstBlock(SM_FileHandle *fHandle, SM_PageHandle memPage);
+extern RC readPreviousBlock(SM_FileHandle *fHandle, SM_PageHandle memPage);
+extern RC readCurrentBlock(SM_FileHandle *fHandle, SM_PageHandle memPage);
+extern RC readNextBlock(SM_FileHandle *fHandle, SM_PageHandle memPage);
+extern RC readLastBlock(SM_FileHandle *fHandle, SM_PageHandle memPage);
 
-/* writing blocks to a page file */
-extern RC writeBlock (int pageNum, SM_FileHandle *fHandle, SM_PageHandle memPage);
-extern RC writeCurrentBlock (SM_FileHandle *fHandle, SM_PageHandle memPage);
-extern RC appendEmptyBlock (SM_FileHandle *fHandle);
-extern RC ensureCapacity (int numberOfPages, SM_FileHandle *fHandle);
+/* Writing blocks to a page file. */
+extern RC writeBlock(int pageNum, SM_FileHandle *fHandle,
+                     SM_PageHandle memPage);
+extern RC writeCurrentBlock(SM_FileHandle *fHandle, SM_PageHandle memPage);
+extern RC appendEmptyBlock(SM_FileHandle *fHandle);
+extern RC ensureCapacity(int numberOfPages, SM_FileHandle *fHandle);
+//endregion
 
 #endif
