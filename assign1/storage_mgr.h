e0a24cfca7faf8e7af6aeeb5ad43b5c4bb14b97e
Hendra Anggrian <hendraanggrian@gmail.com>
2023-06-29 15:15:16 -0500

    Migrated from assign1-0629

--- assign1/storage_mgr.h
+++ assign1/storage_mgr.h
@@ -3,37 +3,41 @@
 
 #include "dberror.h"
 
-// A file handle represents an open page file.
+/************************************************************
+ *                    handle data structures                *
+ ************************************************************/
 typedef struct SM_FileHandle {
-  char *fileName;
-  int totalNumPages;
-  int curPagePos;
-  void *mgmtInfo;
+	char *fileName;
+	int totalNumPages;
+	int curPagePos;
+	void *mgmtInfo;
 } SM_FileHandle;
 
-// A page handle is a pointer to an area in memory storing the data of a page.
-typedef char *SM_PageHandle;
+typedef char* SM_PageHandle;
 
-extern void initStorageManager(void);
+/************************************************************
+ *                    interface                             *
+ ************************************************************/
+/* manipulating page files */
+extern void initStorageManager (void);
+extern RC createPageFile (char *fileName);
+extern RC openPageFile (char *fileName, SM_FileHandle *fHandle);
+extern RC closePageFile (SM_FileHandle *fHandle);
+extern RC destroyPageFile (char *fileName);
 
-/* File related methods */
-extern RC createPageFile(char *fileName);
-extern RC openPageFile(char *fileName, SM_FileHandle *fHandle);
-extern RC closePageFile(SM_FileHandle *fHandle);
-extern RC destroyPageFile(char *fileName);
+/* reading blocks from disc */
+extern RC readBlock (int pageNum, SM_FileHandle *fHandle, SM_PageHandle memPage);
+extern int getBlockPos (SM_FileHandle *fHandle);
+extern RC readFirstBlock (SM_FileHandle *fHandle, SM_PageHandle memPage);
+extern RC readPreviousBlock (SM_FileHandle *fHandle, SM_PageHandle memPage);
+extern RC readCurrentBlock (SM_FileHandle *fHandle, SM_PageHandle memPage);
+extern RC readNextBlock (SM_FileHandle *fHandle, SM_PageHandle memPage);
+extern RC readLastBlock (SM_FileHandle *fHandle, SM_PageHandle memPage);
 
-/* Read and write methods */
-extern RC readBlock(int pageNum, SM_FileHandle *fHandle, SM_PageHandle memPage);
-extern int getBlockPos(SM_FileHandle *fHandle);
-extern RC readFirstBlock(SM_FileHandle *fHandle, SM_PageHandle memPage);
-extern RC readLastBlock(SM_FileHandle *fHandle, SM_PageHandle memPage);
-extern RC readPreviousBlock(SM_FileHandle *fHandle, SM_PageHandle memPage);
-extern RC readCurrentBlock(SM_FileHandle *fHandle, SM_PageHandle memPage);
-extern RC readNextBlock(SM_FileHandle *fHandle, SM_PageHandle memPage);
-extern RC writeBlock(int pageNum, SM_FileHandle *fHandle,
-                     SM_PageHandle memPage);
-extern RC writeCurrentBlock(SM_FileHandle *fHandle, SM_PageHandle memPage);
-extern RC appendEmptyBlock(SM_FileHandle *fHandle);
-extern RC ensureCapacity(int numberOfPages, SM_FileHandle *fHandle);
+/* writing blocks to a page file */
+extern RC writeBlock (int pageNum, SM_FileHandle *fHandle, SM_PageHandle memPage);
+extern RC writeCurrentBlock (SM_FileHandle *fHandle, SM_PageHandle memPage);
+extern RC appendEmptyBlock (SM_FileHandle *fHandle);
+extern RC ensureCapacity (int numberOfPages, SM_FileHandle *fHandle);
 
 #endif
